3fe1e1222c903603c24e860c7db3bbd55315b38e
“Pinchuns” <pinchuns22@gmail.com>
2026-09-08 23:13:43 +0800

    modified pandas amd add file

---
+++ file.py
@@ -0,0 +1,18 @@
+# Reading and Writing Files with Open
+
+#open()
+
+file1 = open("file path", "r")    #file path like /resources/data/example1
+"""
+file path Example: "/resources/data/example1"
+mode
+r = read
+w = write
+a = append
+"""
+
+"""
+file1.name    獲取文件名稱
+file1.mode    確認文件處於什麼模式 r=read
+file1.close() 關閉文件
+"""
--- pandas.py
+++ pandas.py
@@ -1,64 +1,35 @@
 import pandas as pd
-import numpy as np
 
-sales = pd.Series([120, 350, 80, 500, 250])
-print(sales)
 
-sales2 = pd.Series(
-    [120, 350, 80],
-    index=["Jan", "Feb", "Mar"]
-)
-print(sales2)
+# Pandas: Loading Data
 
-# Numpy Array
-numpy_sales = np.array([120, 350, 80])
-print("NumPy:")
-print(numpy_sales)
-print(numpy_sales[1])
 
-# Pandas Series
-pandas_sales = pd.Series([120, 350, 80], index = ["Jan", "Feb", "Mar"], name = "sales3")
-print("Pandas:")
-print(pandas_sales)
-print(pandas_sales["Feb"])
-print(pandas_sales["Jan"])
+Pandas
+read_csv()
+Series()
+DataFrame
+values
+...
+...
+...
 
-# DataFrame
-data = {
-    "name": ["Alice", "Bob", "John"],
-    "city": ["Taipei", "London", "Tokyo"],
-    "sales": [850, 500, 720]
+
+# Importing
+csv_path = 'file1.csv'
+df = pd.read_csv(csv_path)    # df=dataframe縮寫
+df.head()    # 讀取dataframe前五行
+
+# read excel
+xlsx_path = 'file1.xlsx'
+df = pd.read_excel(xlsx_path)
+df.head()
+
+
+# 用dict創建DF
+
+sales = {'city':['Taipei','London','Berlin'],\
+         'sale':[1000, 5000, 550],\
+         'date':['20250809','20250910','20250917']
 }
-df = pd.DataFrame(data)
-print(df)
-print(df["name"])
-print(df["sales"])
-print(df["sales"].mean())
-print(df.shape)
-print(df.columns)
-print(df.head(2))
-
-# Little Test
-data1 = {
-    "name1": ["Alice", "Bob", "John"],
-    "city1": ["Taipei", "London", "Tokyo"],
-    "sales1": [850, 500, 720]
-}
-df1 = pd.DataFrame(data1)
-print(df1["sales1"])
-print(df1["sales1"].mean()) #()call the function
-print(df1.shape)
-print(df1.columns)
-print(df1.head(2))
-
-# Pandas (data filtering)
-data2 = {
-    "name2": ["Alice", "Bob", "John"],
-    "city2": ["Taipei", "London", "Tokyo"],
-    "sales2": [850, 500, 720]
-}
-df2 = pd.DataFrame(data2)
-print(df2[df2["sales2"] >= 700]) #like SQL "WHERE sales >= 700;"
-print(df2[(df2["sales2"] >= 700) & (df2["city2"] == "Taipei")]) #like SQL "WHERE sales >= 700 AND city = 'Taipei';"
-print(df2[(df2["sales2"] >= 700) | (df2["city2"] == "London")]) #like SQL "WHERE sales >= 700 OR city = 'London'"
-print(df2[["name2", "sales2"]])
+sales_fram = pd.DataFrame(sales)
+print(sales_fram)
